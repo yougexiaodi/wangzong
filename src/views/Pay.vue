b881af7b5381befffe0db793c79fd7a49bfa540b
李子豪 <15836784857@163.com>
2019-10-09 18:21:39 +0800

    活动日期更改

--- src/views/Pay.vue
+++ src/views/Pay.vue
@@ -5,7 +5,7 @@
 <!--      <img src="../assets/img/rules.png" width="96%">-->
 <!--    </div>-->
     <div class="details">
-      <div><span>活动时间:&nbsp;&nbsp;</span>2019年9月30日-10月7日</div>
+      <div><span>活动时间:&nbsp;&nbsp;</span>即日起至-2020年6月30日</div>
       <div><span>活动对象:&nbsp;&nbsp;</span>中国银行信用卡客户</div>
       <div><span>活动内容:&nbsp;&nbsp;</span>活动期间，中国银行信用卡客户可在活动期间抢购逸天城美食优惠代金券，使用中国银行信用卡通过“中国银行贵州分行”微信公众号，选择中行信用卡付款，付款完成后持电子券到逸天城2F顾客服务中心兑换商场纸质代金券，到指定商户门店使用，每日名额有限，先到先得。券码在活动期间内有效。</div>
     </div>
