ac5e034e833c191349f843231225680e7a903142
李子豪 <15836784857@163.com>
2019-12-11 09:30:16 +0800

    底部Tab调整

--- src/App.vue
+++ src/App.vue
@@ -2,14 +2,14 @@
     <div id="app">
         <router-view/>
         <template v-if="isShowFooterMenu">
-            <div style="width: 100vw;height: 53px;"></div>
+            <div style="width: 100vw;height: 50px;"></div>
             <tabbar class="ecard-tabbar" style="position:fixed;">
                 <tabbar-item :link="{path:'/'}" :selected="$route.path === '/'">
-                    <span class="ecard-font" style="font-size: 22px" slot="icon">&#xe600;</span>
+                    <span class="icon-home" slot="icon"></span>
                     <span slot="label">首页</span>
                 </tabbar-item>
                 <tabbar-item :link="{path: '/coupon'}" :selected="$route.path === '/coupon'">
-                    <span class="ecard-font" style="font-size: 22px" slot="icon">&#xe601;</span>
+                    <span class="icon-my" slot="icon"></span>
                     <span slot="label">我的</span>
                 </tabbar-item>
             </tabbar>
@@ -40,6 +40,55 @@
         methods: {}
     }
 </script>
+
+<style scoped lang="less">
+
+    /deep/ .weui-tabbar__item {
+        padding-top: 2vw;
+    }
+
+    /deep/ .weui-tabbar__item .weui-tabbar__icon {
+        height: 5vw;
+        width: 5vw;
+
+        .icon-home {
+            display: block;
+            height: 100%;
+            width: 100%;
+            background: url("./assets/icon-home.png") no-repeat;
+            background-size: contain;
+        }
+
+        .icon-my {
+            display: block;
+            height: 100%;
+            width: 100%;
+            background: url("./assets/icon-my.png") no-repeat;
+            background-size: contain;
+        }
+    }
+
+    /deep/ .weui-tabbar__item.weui-bar__item_on .weui-tabbar__icon {
+        .icon-home {
+            background: url("./assets/icon-home-check.png") no-repeat;
+            background-size: contain;
+        }
+
+        .icon-my {
+            background: url("./assets/icon-my-check.png") no-repeat;
+            background-size: contain;
+        }
+    }
+
+    /deep/ .weui-tabbar__item .weui-tabbar__label {
+        color: #c0c0c0;
+    }
+
+    /deep/ .weui-tabbar__item.weui-bar__item_on .weui-tabbar__label {
+        color: #d81e06;
+    }
+
+</style>
 
 <style lang="less">
     @import './assets/css/common.css';
