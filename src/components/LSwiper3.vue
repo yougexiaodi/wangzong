e2c80577c4f13ba74f6e3163cd90d08bca11cdd2
李子豪 <15836784857@163.com>
2019-12-13 17:12:17 +0800

    轮播bug修复

--- src/components/LSwiper3.vue
+++ src/components/LSwiper3.vue
@@ -40,9 +40,17 @@
                     stopOnLastSlide: false,
                     disableOnInteraction: false,
                 },
+                effect : 'coverflow',
+                coverflowEffect: {
+                    rotate: 0,
+                    stretch: 0,
+                    depth: 300,
+                    modifier: 1,
+                    slideShadows : false
+                },
                 centeredSlides: true,
-                initialSlide: 2,
-                watchSlidesProgress: true,
+                // initialSlide: 2,
+                // watchSlidesProgress: true,
                 pagination: {
                     el: this.$refs["swiper-pagination"],
                 },
@@ -51,31 +59,6 @@
                         let realIndex = this.realIndex;
                         _this.handleClick(_this.list[realIndex]);
                     },
-                    progress() {
-                        for (let i = 0; i < this.slides.length; i++) {
-                            let slide = this.slides.eq(i);
-                            let slideProgress = this.slides[i].progress;
-                            let modify = "";
-                            if (Math.abs(slideProgress) > 1) {
-                                modify = (Math.abs(slideProgress) - 1) * 0.4 + 1;
-                            }
-                            let translate = slideProgress * modify * 318 + 'px';
-                            let scale = 1 - Math.abs(slideProgress) / 5;
-                            let zIndex = 999 - Math.abs(Math.round(10 * slideProgress));
-                            slide.transform('translateX(' + translate + ') scale(' + scale + ')');
-                            slide.css('zIndex', zIndex);
-                            slide.css('opacity', 1);
-                            if (Math.abs(slideProgress) > 3) {
-                                slide.css('opacity', 0);
-                            }
-                        }
-                    },
-                    setTransition: function (transition) {
-                        for (let i = 0; i < this.slides.length; i++) {
-                            let slide = this.slides.eq(i);
-                            slide.transition(transition);
-                        }
-                    }
                 },
             })
         },
