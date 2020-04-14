c20191fa1ce2274e00f24634d6a84be1fdfcda2b
李子豪 <15836784857@163.com>
2020-04-14 23:03:29 +0800

    非中国银行卡自动退款

--- src/views/OrderInfo.vue
+++ src/views/OrderInfo.vue
@@ -1,11 +1,11 @@
 <template>
-    <div>
-        <msg :title="title" :description="description" :buttons="buttons" :icon="icon">
-            <p slot="description" style="color: #1aad19;font-size: 22px">{{code}}</p>
-        </msg>
-        <form-preview header-label="付款金额" :header-value="price" :body-items="list" :footer-buttons="buttons2"
-                      name="demo"></form-preview>
-    </div>
+  <div>
+    <msg :title="title" :description="description" :buttons="buttons" :icon="icon">
+      <p slot="description" style="color: #1aad19;font-size: 22px">{{code}}</p>
+    </msg>
+    <form-preview header-label="付款金额" :header-value="price" :body-items="list" :footer-buttons="buttons2"
+                  name="demo"></form-preview>
+  </div>
 </template>
 <script>
     import {Msg, TransferDomDirective as TransferDom, XButton, FormPreview, Box} from 'vux'
@@ -50,28 +50,33 @@
                 this.$http.post(this.getDataUrl, {
                     order_id: this.$route.query.order_id,
                     pid: sessionStorage.getItem('pid')
-                })
-                    .then((res) => {
-                        if (res.data.status === 0) {
-                            this.title = '支付成功'
-                            this.icon = 'success'
-                            this.description = '凭此优惠码可前往指定门店享受优惠'
-                            this.code = res.data.info.code
-                            this.price = '￥' + res.data.info.pay_price
-                            this.list = [
-                                {label: '商户名称', value: res.data.info.merchant},
-                                {label: '商品名称', value: res.data.info.name},
-                                {label: '优惠码', value: res.data.info.code},
-                                {label: '订单号', value: res.data.info.order_id},
-                                {label: '支付方式', value: res.data.info.pay_type},
-                                {label: '交易时间', value: res.data.info.pay_time}
-                            ]
-                        } else {
-                            this.title = '支付失败'
-                            this.icon = 'warn'
-                            this.description = '支付失败，请重新支付'
+                }).then((res) => {
+                    if (res.data.status === 0) {
+                        this.title = '支付成功'
+                        this.icon = 'success'
+                        this.description = '凭此优惠码可前往指定门店享受优惠'
+                        this.code = res.data.info.code
+                        this.price = '￥' + res.data.info.pay_price
+                        this.list = [
+                            {label: '商户名称', value: res.data.info.merchant},
+                            {label: '商品名称', value: res.data.info.name},
+                            {label: '优惠码', value: res.data.info.code},
+                            {label: '订单号', value: res.data.info.order_id},
+                            {label: '支付方式', value: res.data.info.pay_type},
+                            {label: '交易时间', value: res.data.info.pay_time}
+                        ]
+                        if (res.data.info.state === '4') {
+                            this.$vux.alert.show({
+                                title: "温馨提示",
+                                content: "您选择了非中行信用卡支付，所支付金额将在24小时内原路退回。名额将自动释放，请您选择中行信用卡继续支付购买。",
+                            })
                         }
-                    })
+                    } else {
+                        this.title = '支付失败'
+                        this.icon = 'warn'
+                        this.description = '支付失败，请重新支付'
+                    }
+                })
             },
             getLoginState() {
                 this.$vux.loading.show({
