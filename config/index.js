721c1b09280d14b687af3433b9244a87a4f075c7
ext.wanxiaodi1 <ext.wanxiaodi1@jd.com>
2022-12-30 17:22:03 +0800

    show

--- config/index.js
+++ config/index.js
@@ -58,7 +58,7 @@ module.exports = {
         // Paths
         assetsRoot: path.resolve(__dirname, '../dist'),
         assetsSubDirectory: 'static',
-        assetsPublicPath: '/boc/hebei_lifecycle/',
+        assetsPublicPath: './',
 
         /**
          * Source Maps
